2ab1609ca434653ba4dce718165ad97107bb9d6d
Jason Bobich <jasonbobich@gmail.com>
2013-05-05 12:24:55 -0800

    Added smoothheight param to [post_slider] shortcode.

--- frontend/sliders-display.php
+++ frontend/sliders-display.php
@@ -274,7 +274,7 @@ function themeblvd_slider_fallback_default( $slider, $slides, $fallback ) {
 /**
  * Custom slider
  *
- * @since 2.0.0
+ * @since 1.0.0
  *
  * @param array $atts Standard WordPress shortcode attributes
  */
@@ -305,7 +305,7 @@ function themeblvd_shortcode_slider( $atts ) {
 /**
  * Post slider shortcode - [post_slider]
  *
- * @since 2.0.2
+ * @since 1.0.2
  *
  * @param array $atts Standard WordPress shortcode attributes
  */
@@ -316,23 +316,25 @@ function themeblvd_shortcode_slider_auto( $atts ) {
 		return __( 'Your theme does not support the [post_slider] shortcode.', 'themeblvd_sliders' );
 	
 	$default = array(
-		'fx' 				=> 'slide', 	// Effect for transitions
-		'timeout' 			=> '3',			// Time between auto trasitions in seconds
-		'nav_standard' 		=> '1',			// Show standard nav - true, false
-		'nav_arrows'		=> '1',			// Show nav arrows - true, false
-		'pause_play'		=> '1',			// Show pause/play buttons - true, false
-		'pause_on_hover' 	=> 'disable',	// Pause on hover - pause_on, pause_on_off, disable
-		'image' 			=> 'full',		// How to display featured images - full, align-right, align-left
-		'image_link' 		=> 'permalink',	// Where image link goes - permalink, lightbox, none
-		'button' 			=> '',			// Text for button to lead to permalink - leave empty to hide
-		'tag' 				=> '',			// Tag(s) to include/exclude
-		'category_name'		=> '',			// Category slug(s) to include/exclude
-		'cat'				=> '',			// Category ID(s) to include/exclude
-		'numberposts' 		=> '5',			// Number of posts/slides
-		'orderby' 			=> 'date',		// Orderby param for posts query
-		'order'				=> 'DESC',		// Order param for posts query
-		'query' 			=> '',			// Custom query string
-		'mobile_fallback' 	=> 'full_list'	// How to display on mobile - full_list, first_slide, display
+		'fx' 				=> 'slide', 		// Effect for transitions - slide, fade
+		'smoothheight' 		=> 'true',			// Enable smoothheight - true, false
+		'timeout' 			=> '3',				// Time between auto trasitions in seconds
+		'nav_standard' 		=> '1',				// Show standard nav - true, false
+		'nav_arrows'		=> '1',				// Show nav arrows - true, false
+		'pause_play'		=> '1',				// Show pause/play buttons - true, false
+		'pause_on_hover' 	=> 'disable',		// Pause on hover - pause_on, pause_on_off, disable
+		'image' 			=> 'full',			// How to display featured images - full, align-right, align-left
+		'image_size'		=> 'slider-large', 	// Crop size for full-size images
+		'image_link' 		=> 'permalink',		// Where image link goes - permalink, lightbox, none
+		'button' 			=> '',				// Text for button to lead to permalink - leave empty to hide
+		'tag' 				=> '',				// Tag(s) to include/exclude
+		'category_name'		=> '',				// Category slug(s) to include/exclude
+		'cat'				=> '',				// Category ID(s) to include/exclude
+		'numberposts' 		=> '5',				// Number of posts/slides
+		'orderby' 			=> 'date',			// Orderby param for posts query
+		'order'				=> 'DESC',			// Order param for posts query
+		'query' 			=> '',				// Custom query string
+		'mobile_fallback' 	=> 'full_list'		// How to display on mobile - full_list, first_slide, display
 	);
     $atts = shortcode_atts( $default, $atts );
 
